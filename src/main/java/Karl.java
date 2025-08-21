12da2e60597b710372a7eda64741a4c7b19baa8d
rayner <raynergoh03@gmail.com>
2025-08-21 15:11:01 +0800

    echoes commands made by user, exits when user types bye

--- src/main/java/Karl.java
+++ src/main/java/Karl.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Karl {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Karl");
+        Scanner sc = new Scanner(System.in);
+
+        // chatbot intro
+        printLine();
+        System.out.println(" Hello! I'm Karl 🤖");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+        System.out.println();
+
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                printLine();
+                System.out.println(" Bye. Karl hopes to see you again soon!");
+                printLine();
+                break;
+            }
+
+            // echo user input
+            printLine();
+            System.out.println(" " + input);
+            printLine();
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
+    // Helper function for formatting
+    private static void printLine() {
         System.out.println("____________________________________________________________");
     }
 }
