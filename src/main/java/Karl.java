82f48a9ef9127ab5d97c2bf115e12f7841fbe502
rayner <raynergoh03@gmail.com>
2025-08-21 15:23:56 +0800

    added the ability to mark tasks as done, and the ability to unmark tasks

--- src/main/java/Karl.java
+++ src/main/java/Karl.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Karl {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>(); // dynamic list for tasks
+        ArrayList<Task> tasks = new ArrayList<>(); // dynamic list of Task objects
 
         // chatbot intro
         printLine();
@@ -23,22 +22,50 @@ public class Karl {
                 printLine();
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                // print all tasks
+                // print task list
                 printLine();
                 if (tasks.isEmpty()) {
                     System.out.println(" (No tasks yet!)");
                 } else {
+                    System.out.println(" Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println(" " + (i + 1) + ". " + tasks.get(i));
                     }
                 }
                 printLine();
                 System.out.println();
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task task = tasks.get(index);
+                    task.markAsDone();
+                    printLine();
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + task);
+                    printLine();
+                } catch (Exception e) {
+                    System.out.println(" Invalid task number!");
+                }
+                System.out.println();
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task task = tasks.get(index);
+                    task.markAsNotDone();
+                    printLine();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + task);
+                    printLine();
+                } catch (Exception e) {
+                    System.out.println(" Invalid task number!");
+                }
+                System.out.println();
             } else {
                 // add new task
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 printLine();
-                System.out.println(" added: " + input);
+                System.out.println(" added: " + newTask);
                 printLine();
                 System.out.println();
             }
