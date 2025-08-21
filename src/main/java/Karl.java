5114966173768dc5ebc7e699405e7dbbea428396
rayner <raynergoh03@gmail.com>
2025-08-21 15:16:04 +0800

    ability to add tasks and display them when requested

--- src/main/java/Karl.java
+++ src/main/java/Karl.java
@@ -4,6 +4,9 @@ public class Karl {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] tasks = new String[100]; // fixed-size array for tasks
+        int taskCount = 0;               // keeps track of how many tasks are stored
+
         // chatbot intro
         printLine();
         System.out.println(" Hello! I'm Karl 🤖");
@@ -19,19 +22,33 @@ public class Karl {
                 System.out.println(" Bye. Karl hopes to see you again soon!");
                 printLine();
                 break;
-            }
+            } else if (input.equalsIgnoreCase("list")) {
+                // print all tasks
+                printLine();
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                if (taskCount == 0) {
+                    System.out.println(" (No tasks yet!)");
+                }
+                printLine();
+                System.out.println();
+            } else {
+                // store the task in array
+                tasks[taskCount] = input;
+                taskCount++;
 
-            // echo user input
-            printLine();
-            System.out.println(" " + input);
-            printLine();
-            System.out.println();
+                printLine();
+                System.out.println(" added: " + input);
+                printLine();
+                System.out.println();
+            }
         }
 
         sc.close();
     }
 
-    // Helper function for formatting
+    // helper formatter
     private static void printLine() {
         System.out.println("____________________________________________________________");
     }
